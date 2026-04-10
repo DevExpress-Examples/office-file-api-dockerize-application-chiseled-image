f27bab6368cb4e90877d5b0b3ed33cd57b52cbac
Anton Sameishchev <anton.sameishchev@devexpress.com>
2026-04-10 11:18:42 +0200

    Refactor fontFiles initialization for clarity

--- VB/Program.vb
+++ VB/Program.vb
@@ -10,9 +10,7 @@ Friend Module Program
             Dim docxPath = If(args.Length >= 1, args(0), Path.Combine(baseDir, "fontTest.docx"))
             ' Include Fonts in the Application
             Dim fonts As String() = {"Inter-Regular.ttf", "NotoSans-Regular.ttf"}
-            Dim fontFiles As String() = fonts.
-                Select(Function(f) Path.Combine(AppContext.BaseDirectory, f)).
-                ToArray()
+            Dim fontFiles As String() = fonts.Select(Function(f) Path.Combine(AppContext.BaseDirectory, f)).ToArray()
             ' Register fonts before loading documents
             For Each fp In fontFiles
                 Call DXFontRepository.Instance.AddFont(fp)
